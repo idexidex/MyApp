9274ba16ca9fefa15c17c9878e4367da52fc1961
idexidex <s9500062747@gmail.com>
2014-06-03 15:12:30 +0400

    old time ?

--- src/com/mycompany/myapp/MainActivity.java
+++ src/com/mycompany/myapp/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends Activity {
 		btnOk = (Button) findViewById(R.id.btnOk);
 		EditText= (EditText) findViewById(R.id.EditText);
 		
+		
 		// создаем обработчик нажатия
 		
 		OnClickListener oclBtnOk = new OnClickListener() {
@@ -53,7 +54,7 @@ public class MainActivity extends Activity {
 				//df.format(0.912385);
 				
 				
-					tvOut.setText(""+ df.format(v)+ "КМ/Ч");
+					tvOut.setText(""+ df.format(v)+ " КМ/Ч");
 				}  
 				
 				oldTime=time;
@@ -66,18 +67,40 @@ public class MainActivity extends Activity {
 		// присвоим обработчик кнопке OK (btnOk) иии
 		btnOk.setOnClickListener(oclBtnOk);
 
-		
-}
+		}
 		@Override
 
 	public boolean  onKeyDown(int keyCode, KeyEvent event) {
-		// Обработайте нажатие, верните true, если обработка выполнена
+			//Обработайте нажатие, верните true, если обработка выполнена
+			 long time = 0;
+			 long oldTime = 1;
+			 long delta = 0;
+
+			 int s; 
+			 time  = System.currentTimeMillis();
+			 delta = time - oldTime;
+			 //if (oldTime > 0) {   Как послеэтого ставить return
+			 float b = (float) 0.000000278;// часов в одной милисек
+			 float h = delta * b;
+			 float ss = (float) 0.002089;//длина окружности колеса 16 " в КМ
+			 float sss= (float) 0.000001;// мм в км
+			 float v = ss/h;
+			 DecimalFormat df = new DecimalFormat("#.#");
+
+			 
+			
+			
 		if (keyCode == KeyEvent.KEYCODE_INFO)
+			//btnOk.performClick(); //тормозит при быстром нажатии
 		{
-			tvOut.setText("ok");
+			tvOut.setText(""+ df.format(v)+ " КМ/Ч");
+			
+			oldTime=time;
+			
 		return true;
 		
 			} 
 	 return super.onKeyDown(keyCode, event);
 	 }
+	 
 	 }
