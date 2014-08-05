6cddb26649445c2c929d528d93d7b6395946d940
idexidex <s9500062747@gmail.com>
2014-08-05 19:40:59 +0400

    добавил счётчик расстояние

--- src/com/mycompany/myapp/MainActivity.java
+++ src/com/mycompany/myapp/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends Activity {
 	Button btnOk;
 	Button btnCancel;
 	EditText EditText;
+	TextView distanse;
+	
 	
 	
 	
@@ -26,15 +28,26 @@ public class MainActivity extends Activity {
 		tvOut = (TextView) findViewById(R.id.tvOut);
 		btnOk = (Button) findViewById(R.id.btnOk);
 		EditText= (EditText) findViewById(R.id.EditText);
+		distanse = (TextView) findViewById(R.id.distanse);
+	/* @Override
+	public boolean  onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_INFO)
+		btnOk.performClick(); //тормозит при быстром нажатии
+		return true;
+
+	} 
+	return super.onKeyDown(keyCode, event);
+	}
 		
-		
+		*/
 		// создаем обработчик нажатия
+		
 		
 		OnClickListener oclBtnOk = new OnClickListener() {
 			long time = 0;
 			long oldTime = 0;
 			long delta = 0;
-			
+			float ddistanse = 0;
 			int s; 
 			
 			@Override
@@ -50,11 +63,17 @@ public class MainActivity extends Activity {
 				float ss = (float) 0.002089;//длина окружности колеса 16 " в КМ
 				float sss= (float) 0.000001;// мм в км
 				float v = ss/h;
+				ddistanse = ss + ddistanse;
 				DecimalFormat df = new DecimalFormat("#.#");
+					DecimalFormat rf = new DecimalFormat("#.###");
+
+					
+				
 				//df.format(0.912385);
 				
 				
 					tvOut.setText(""+ df.format(v)+ " КМ/Ч");
+					distanse.setText("" + rf.format(ddistanse) + "КМ");
 				}  
 				
 				oldTime=time;
@@ -71,36 +90,42 @@ public class MainActivity extends Activity {
 		@Override
 
 	public boolean  onKeyDown(int keyCode, KeyEvent event) {
-			//Обработайте нажатие, верните true, если обработка выполнена
-			 long time = 0;
-			 long oldTime = 1;
-			 long delta = 0;
+			/*/Обработайте нажатие, верните true, если обработка выполнена
+			  
+			long time = 0;
+			long oldTime = 0;
+			long delta = 0;
 
-			 int s; 
-			 time  = System.currentTimeMillis();
-			 delta = time - oldTime;
-			 //if (oldTime > 0) {   Как послеэтого ставить return
-			 float b = (float) 0.000000278;// часов в одной милисек
-			 float h = delta * b;
-			 float ss = (float) 0.002089;//длина окружности колеса 16 " в КМ
-			 float sss= (float) 0.000001;// мм в км
-			 float v = ss/h;
-			 DecimalFormat df = new DecimalFormat("#.#");
+			int s; 
+			time  = System.currentTimeMillis();
+			delta = time - oldTime;
+			if (oldTime > 0) {  // Как послеэтого ставить return
+				//return true;
+				//}
+				float b = (float) 0.000000278;// часов в одной милисек
+				float h = delta * b;
+				float ss = (float) 0.002089;//длина окружности колеса 16 " в КМ
+				float sss= (float) 0.000001;// мм в км
+				float v = ss/h;
+				DecimalFormat df = new DecimalFormat("#.#");
 
-			 
+				*/
 			
-			
-		if (keyCode == KeyEvent.KEYCODE_INFO)
-			//btnOk.performClick(); //тормозит при быстром нажатии
+		if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN )//  INFO)
+			btnOk.performClick(); //тормозит при быстром нажатии
 		{
-			tvOut.setText(""+ df.format(v)+ " КМ/Ч");
+				
+			/*tvOut.setText(""+ df.format(v)+ " КМ/Ч");
 			
-			oldTime=time;
-			
-		return true;
+			oldTime=time;*/
+			}
+			return true;
 		
-			} 
-	 return super.onKeyDown(keyCode, event);
-	 }
+			}
+			
+	// return super.onKeyDown(keyCode, event);
+	 
+	 
+	 
 	 
 	 }
